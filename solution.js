2f2ce8bbc915a36bef749e920fc84d3ed89def58
rrroksiii <natasonata94@gmail.com>
2018-05-22 13:48:57 +0300

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
 import SweetScroll from 'sweet-scroll';
-
+alert("Я работаю!");
 document.addEventListener('DOMContentLoaded', () => {
   const sweetScroll = new SweetScroll({ horizontal: true });
 }, false);
